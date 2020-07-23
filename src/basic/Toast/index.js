e31be7632287318bb04e94fe22aaf5de534bd5ad
Kevin Cheng <kevncheng@gmail.com>
2020-07-23 14:28:46 -0400

    change starting height + 50

--- src/basic/Toast/index.js
+++ src/basic/Toast/index.js
@@ -1,75 +1,93 @@
-import React, { Component } from 'react'
-import { View, Animated, Text, StyleSheet, Image, Dimensions } from 'react-native'
+import React, { Component } from 'react';
+import {
+    View,
+    Animated,
+    Text,
+    StyleSheet,
+    Image,
+    Dimensions
+} from 'react-native';
 
-const { height } = Dimensions.get('screen')
+const { height } = Dimensions.get('screen');
 
 class Toast extends Component {
-    static toastInstance
+    static toastInstance;
 
     static show({ ...config }) {
-		this.toastInstance.start(config)
-    }
-    
-    static hide() {
-		this.toastInstance.hideToast()
-    }
-    
-    state = {
-        toast: new Animated.Value(height)
+        this.toastInstance.start(config);
     }
 
-    start({ ...config }){
+    static hide() {
+        this.toastInstance.hideToast();
+    }
+
+    state = {
+        toast: new Animated.Value(height + 50)
+    };
+
+    start({ ...config }) {
         this.setState({
             title: config.title,
             text: config.text,
             color: config.color,
             icon: config.icon,
             timing: config.timing
-        })
+        });
 
         Animated.spring(this.state.toast, {
             toValue: height - 130,
             bounciness: 15,
             useNativeDriver: true
-        }).start()
+        }).start();
 
-        const duration = config.timing > 0 ? config.timing : 5000
+        const duration = config.timing > 0 ? config.timing : 5000;
 
         setTimeout(() => {
-            this.hideToast()
-        }, duration)
+            this.hideToast();
+        }, duration);
     }
 
-    hideToast(){
+    hideToast() {
         Animated.timing(this.state.toast, {
-            toValue: height,
+            toValue: height + 50,
             duration: 300,
             useNativeDriver: true
-        }).start()
+        }).start();
     }
 
-    render(){
-        const { title, text, icon, color } = this.state
-        return(
-            <Animated.View 
-                ref={c => this._root = c}
-                style={[styles.toast, {
-                    transform: [
-                        { translateY: this.state.toast }
-                    ]
-                }]}
+    render() {
+        const { title, text, icon, color } = this.state;
+        return (
+            <Animated.View
+                ref={c => (this._root = c)}
+                style={[
+                    styles.toast,
+                    {
+                        transform: [{ translateY: this.state.toast }]
+                    }
+                ]}
             >
-                <View style={[styles.timing, { backgroundColor: color || 'transparent' }]} />
+                <View
+                    style={[
+                        styles.timing,
+                        { backgroundColor: color || 'transparent' }
+                    ]}
+                />
                 <View style={styles.content}>
-                    <Text style={[styles.title, { color }]}>{ title }</Text>
-                    <Text style={styles.subtitle}>{ text }</Text>
+                    <Text style={[styles.title, { color }]}>{title}</Text>
+                    <Text style={styles.subtitle}>{text}</Text>
                 </View>
 
-                <View style={[styles.iconStatus, { backgroundColor: color || 'transparent' }]}>
-                    { icon }
+                <View
+                    style={[
+                        styles.iconStatus,
+                        { backgroundColor: color || 'transparent' }
+                    ]}
+                >
+                    {icon}
                 </View>
             </Animated.View>
-        )
+        );
     }
 }
 
@@ -81,11 +99,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         borderRadius: 10,
         minHeight: 90,
-        shadowColor: "#ccc",
+        shadowColor: '#ccc',
         alignItems: 'center',
         shadowOffset: {
             width: 0,
-            height: 2,
+            height: 2
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
@@ -99,12 +117,12 @@ const styles = StyleSheet.create({
         backgroundColor: '#f1f1f1',
         position: 'absolute',
         top: 0
-    },  
+    },
     content: {
         width: '90%',
         paddingLeft: 20,
         paddingRight: 20
-    },  
+    },
     title: {
         color: '#f1f1f1',
         fontWeight: '600',
@@ -130,6 +148,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     }
-})
+});
 
-export default Toast
+export default Toast;
